6c959abfd45d9189b8cb25be5547792f1b97da5d
antara-chugh <antara.chugh15@gmail.com>
2024-10-27 19:24:58 -0700

    ready to submit

--- README.md
+++ README.md
@@ -2,16 +2,27 @@
 
 ## Pipe Up
 
-One sentence description
+This code implements the shell redirect operation ("|") via a program pipe. 
 
 ## Building
 
-Explain briefly how to build your program
+To build the program, run make to compile. Then, ./pipe can be run followed by shell commands as arguments, each argument feeding into the next.
 
 ## Running
+Example command:
+./pipe ls cat wc
+This command will print the number of files and directories twice, followed by the total number of characters across all file and directory name and newlines.When running in this project directory after running make, this will output "8 8 73". It takes the output of ls, redirects into cat, which then redirects its output to wc.
 
-Show an example run of your program, using at least two additional arguments, and what to expect
+./pipe ps sort
+sorts the current running processes in alphanumeric order (where numbers are less than letters).
+Example output:
+2892 pts/0 00:00:00 pipe
+2893 pts/0 00:00:00 ps
+716 pts/0  00:00:00 zsh
+PID TTY        TIME CMD
+
+Note that adding flags to commands is not supported.
 
 ## Cleaning up
 
-Explain briefly how to clean up all binary files
+To clean up binary files, run "make clean".
--- pipe.c
+++ pipe.c
@@ -14,22 +14,7 @@ int main(int argc, char *argv[])
     
     return EINVAL;
   }
-   /*
-  if(num_cmds==1){
-    pid_t pid=fork();
-    if(pid<0){
-      
-      return errno;
-    }else if(pid==0){
-      execlp(argv[1], argv[1], (char *)NULL);
-      
-      return errno;
-    }else{
-      waitpid(pid, NULL, 0);
-    }
-    return 0;
-  }
-  */
+   
 
   int prev_output;
   for(int i=1; i<argc; i++){
@@ -78,85 +63,4 @@ int main(int argc, char *argv[])
   }
   return 0;
 }
-/*
-  
-    for(int i=0;i<num_cmds; i++){
-     pid_t pid= fork();
-      if(pid<0){
-	
-	return errno;
-
-      }else if(pid==0){
-	if(i>0){
-
-	  if(dup2(pipes[i-1][0], STDIN_FILENO)==-1){
-	    
-	    return errno;
-	  }
-	  
-
-	}
-	if(i<num_cmds-1){
-	  if(dup2(pipes[i][1], STDOUT_FILENO)==-1){
-	    
-	    return errno;
-	  }
-	}
-
-	for(int j=0; j<num_cmds-1; j++){
-	  close(pipes[j][0]);
-	  close(pipes[j][1]);
-
-	  }
-
-	execlp(argv[i+1], argv[i+1], (char *)NULL);
-	printf("bogus argument shouldn't pass");
-	exit(1);
-	
-	return errno;
-
-
-      }
-      else{
-
-	int status;
-	wait(&status);
-	if(WIFEXITED(status)){
-	  return -1;
-	}
-	if(i>0){
-	close(pipes[i-1][0]);
-	close(pipes[i-1][1]);
-	}
-
-
-      }
-    }
-    return 0;
-      
-
-      //parent process
-    /* for(int i=0; i<num_cmds-1; i++){
-
-	close(pipes[i][0]);
-	close(pipes[i][1]);
-      }
-
-      for(int i=0; i<num_cmds; i++){
-	int status;
-	wait(&status);
-	if(WIFEXITED(status)){
-	  return -1;
-	}
-	
-	
-      }
-
-      printf("ran");
-      return 0;
-    
-    
-    
-}
-*/
   
